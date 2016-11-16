bda590d687f0d13516a79a3b617bdaa238ff6719
broerjuang <juangwiantoro@gmail.com>
2016-11-16 10:02:25 +0700

    all function and testing works as expected

--- index.js
+++ index.js
@@ -1,26 +1,30 @@
-module.exports = {
-  // from given number, check wheter it's prime or not
-  isPrime(num) {
-    for (var i = 2; i <= Math.sqrt(num); i += 1) {
-      if (num % i === 0) {
-        return false;
-      }
-    }
-    return true;
-  },
+'use strict'
 
-  // checking the primeIndex of given number
-
-  primeIndex(num) {
-    var listPrime = [];
-    while (listPrime.length < num) {
-      for (var i = 2; i < num * num; i += 1) {
-        if (checkPrime(i)) {
-          listPrime.push(i);
-        }
-      }
+// from given number, check wheter it's prime or not
+let isPrime = (num) => {
+  for (var i = 2; i <= Math.sqrt(num); i += 1) {
+    if (num % i === 0) {
+      return false;
     }
-    return listPrime[num - 1];
   }
+  return true;
+}
 
+// checking the primeIndex of given number
+// giving the index, the function will give you the prime number
+let primeIndex = (num) => {
+  var listPrime = [];
+  while (listPrime.length < num) {
+    for (var i = 2; i < num * num; i += 1) {
+      if (isPrime(i)) {
+        listPrime.push(i);
+      }
+    }
+  }
+  return listPrime[num - 1];
+}
+
+module.exports = {
+  isPrime,
+  primeIndex
 }
